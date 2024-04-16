1b24d6147ecbc99d464979dfed8620457bbaabfb
Sajid Ahamed <sajid13673@gmail.com>
2024-04-16 22:34:21 +0530

    feat: Added loading component and made some design changes to the add borrowing

--- src/Components/Borrowing/addBorrowing.js
+++ src/Components/Borrowing/addBorrowing.js
@@ -5,6 +5,7 @@ import moment from 'moment'
 import React, { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
+import Loading from '../loading'
 function AddBorrowing(props) {
     const location = useLocation();
     const navigate = useNavigate();
@@ -31,10 +32,13 @@ function AddBorrowing(props) {
         options: Array.from(members.data ? members.data : []),
         getOptionLabel: (option) => option.id+'. '+option.name,
       };
+      const [loading, setLoading] = React.useState(false);
       async function getMembers(){
+        setLoading(true);
         await axios.get('http://127.0.0.1:8000/api/member').then((res)=>{
             if(res.data.status){
                 setMembers(res.data.data)
+                setLoading(false);
                 console.log(res.data.data);
             }
         }).catch(err => console.log(err))
@@ -72,80 +76,135 @@ function AddBorrowing(props) {
         const dueDate = moment(date).add(5, 'days').format('YYYY/MM/DD HH:mm:ss');
         setFormData(prevFormData=>{return{...prevFormData, due_date:dueDate}})
       },[])
-      //not needed
-      useEffect(()=>{
-        console.log("formData");
-        console.log(formData);
-      },[formData, book])
       
   return (
     <Grid className="grid" style={{ alignItems: "center" }}>
-      <Container className="input-form" maxWidth="sm">
-        <Grid item xs={12}>
-          <Typography variant='h3'>ADD BORROWING</Typography>
-        </Grid>
-        <Grid container spacing={2}>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Container className="input-form" maxWidth="sm">
           <Grid item xs={12}>
-            <FormControl>
-              <Autocomplete
-              style={{ maxWidth: "24rem", width: "24rem"}}
-
-                {...defaultProps}
-                id="controlled-demo"
-                value={value}
-                onChange={(event, newValue) => handleChange(newValue)}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    label="Select Member"
-                    variant="standard"
-                  />
-                )}
-              />
-              {error && (<div className="error" >Please select a member</div>)}
-            </FormControl>
+            <Typography variant="h3">ADD BORROWING</Typography>
           </Grid>
-          <Grid item xs={12}>
-            <Typography variant='h5'>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <FormControl>
+                <Autocomplete
+                  style={{ maxWidth: "24rem", width: "24rem" }}
+                  {...defaultProps}
+                  id="controlled-demo"
+                  value={value}
+                  onChange={(event, newValue) => handleChange(newValue)}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Select Member"
+                      variant="standard"
+                    />
+                  )}
+                />
+                {error && <div className="error">Please select a member</div>}
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h5">
                 DUE DATE: {moment(formData.due_date).format("MMMM DD YYYY")}
-            </Typography>
-          </Grid>
-          <Grid item xs={6}>
-          <Typography variant='h6'>
-            Book Details
-          </Typography>
-            <Typography align='left'>
-                Book Name: {book.name}
-                <br/>
-                Author: {book.author}
-                <br/>
-                Publisher: {book.publisher}
-                <br/>
-                Year: {book.year}
-            </Typography>
-          </Grid>
-          <Grid item xs={6}>
-          <Typography variant='h6'>
-            Member Details
-          </Typography>
-            <Typography align='left'>
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="h6">Book Details</Typography>
+              <Grid container xs={12} style={{ textAlign: "left" }}>
+              <Grid item xs={4}>
+                  <Typography variant="body1" color="textSecondary">Title:</Typography>
+                </Grid>
+                <Grid item xs={8}>
+                  <Typography variant="body1" color="textSecondary">{book.title}</Typography>
+                </Grid>
+                <Grid item xs={4}>
+                  <Typography variant="body1" color="textSecondary">Author:</Typography>
+                </Grid>
+                <Grid item xs={8}>
+                  <Typography variant="body1" color="textSecondary">{book.author}</Typography>
+                </Grid>
+                <Grid item xs={4}>
+                  <Typography variant="body1" color="textSecondary">Publisher:</Typography>
+                </Grid>
+                <Grid item xs={8}>
+                  <Typography variant="body1" color="textSecondary">{book.publisher}</Typography>
+                </Grid>
+                <Grid item xs={4}>
+                  <Typography variant="body1" color="textSecondary">Year:</Typography>
+                </Grid>
+                <Grid item xs={8}>
+                  <Typography variant="body1" color="textSecondary">{book.year}</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="h6">Book Details</Typography>
+              {value !== null ? (<Grid container xs={12} style={{ textAlign: "left" }}>
+              <Grid item xs={4}>
+                  <Typography variant="body1" color="textSecondary">Name:</Typography>
+                </Grid>
+                <Grid item xs={8}>
+                  <Typography variant="body1" color="textSecondary">{value.name}</Typography>
+                </Grid>
+                <Grid item xs={4}>
+                  <Typography variant="body1" color="textSecondary">Number:</Typography>
+                </Grid>
+                <Grid item xs={8}>
+                  <Typography variant="body1" color="textSecondary">{value.phone_number}</Typography>
+                </Grid>
+                <Grid item xs={4}>
+                  <Typography variant="body1" color="textSecondary">Email:</Typography>
+                </Grid>
+                <Grid item xs={8}>
+                  <Typography variant="body1" color="textSecondary">{value.email}</Typography>
+                </Grid>
+                <Grid item xs={4}>
+                  <Typography variant="body1" color="textSecondary">Status:</Typography>
+                </Grid>
+                <Grid item xs={8}>
+                  <Typography variant="body1" color="textSecondary">{value.status
+                    ? "Active"
+                    : "Inactive"}</Typography>
+                </Grid>
+              </Grid>) : 
+              (<Typography variant="body1" color="textSecondary">
+                Not Selected
+              </Typography>)}
+            </Grid>
+            {/* <Grid item xs={6}>
+              <Typography variant="h6">Member Details</Typography>
+              <Typography align="left">
                 Name: {value !== null ? value.name : "Not Available"}
-                <br/>
-                Phone Number: {value !== null ? value.phone_number : "Not Available"}
-                <br/>
+                <br />
+                Phone Number:{" "}
+                {value !== null ? value.phone_number : "Not Available"}
+                <br />
                 Email: {value !== null ? value.email : "Not Available"}
-                <br/>
-                Status: {value !== null ? value.status ? "Active" : "Inactive" : "Not Available"}
-            </Typography>
+                <br />
+                Status:{" "}
+                {value !== null
+                  ? value.status
+                    ? "Active"
+                    : "Inactive"
+                  : "Not Available"}
+              </Typography>
+            </Grid> */}
+            <Grid item xs={12}>
+              <Button
+                onClick={handleSubmit}
+                variant="contained"
+                color="primary"
+              >
+                Add Borrowing
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Button onClick={handleSubmit} variant="contained" color="primary">
-              Add Borrowing
-            </Button>
-          </Grid>
-        </Grid>
-        {/* </form> */}
-      </Container>
+          {/* </form> */}
+        </Container>
+      )}
     </Grid>
   );
 }
