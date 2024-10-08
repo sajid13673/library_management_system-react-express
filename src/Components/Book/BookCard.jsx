13d7bf1b320b6ed1cc3d479263f15caccab15a6c
Sajid Ahamed <sajid13673@gmail.com>
2024-10-08 17:24:12 +0530

    feat: Integrated role based access to some features

--- src/Components/Book/BookCard.jsx
+++ src/Components/Book/BookCard.jsx
@@ -3,20 +3,32 @@ import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import EditIcon from "@mui/icons-material/Edit";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import BlockIcon from "@mui/icons-material/Block";
-import {  Button,
+import {
+  Button,
   Card,
   CardActions,
   CardContent,
   Grid,
-  Typography, Tooltip } from "@mui/material";
+  Typography,
+  Tooltip,
+} from "@mui/material";
+import { useAuth } from "../../Utils/authProvider";
 
 export default function BookCard(props) {
+  const { token } = useAuth();
   return (
     <Grid item xs={12} sm={12} md={6} lg={4}>
       <Card sx={{ minWidth: 200 }}>
-        <CardContent sx={{ display: 'flex', flexDirection: 'column' ,justifyContent: 'center', alignItems: 'center' }}>
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <img
-            style={{ maxWidth: '100px' }}
+            style={{ maxWidth: "100px" }}
             src={props.path !== null ? props.path : props.defaultImage}
             alt="productImage"
           />
@@ -32,53 +44,76 @@ export default function BookCard(props) {
           <Typography variant="body2">{props.year}</Typography>
         </CardContent>
         <CardActions style={{ justifyContent: "center" }}>
-          <Tooltip title="Edit" placement="top-start">
-            <div>
-              <Button
-                onClick={() => props.handleBookEdit(props.id)}
-                size="small"
-                variant="contained"
-                style={{ backgroundColor: "rgb(0, 172, 14)", color: "white" }}
-              >
-                <EditIcon />
-              </Button>
-            </div>
-          </Tooltip>
-          <Tooltip
-            title={!props.activeBorrowings ? "Delete" : "Has active borrowings"}
-            placement="top-start"
-          >
+          {token.role === "admin" && (
             <>
-              <Button
-                disabled={props.activeBorrowings}
-                size="small"
-                onClick={() => props.handleBookDelete(props.id)}
-                variant="contained"
-                color="secondary"
-                style={{
-                  background: props.activeBorrowings ? "#FFC0C0" : "#E71919",
-                }}
+              <Tooltip title="Edit" placement="top-start">
+                <div>
+                  <Button
+                    onClick={() => props.handleBookEdit(props.id)}
+                    size="small"
+                    variant="contained"
+                    style={{
+                      backgroundColor: "rgb(0, 172, 14)",
+                      color: "white",
+                    }}
+                  >
+                    <EditIcon />
+                  </Button>
+                </div>
+              </Tooltip>
+              <Tooltip
+                title={
+                  !props.activeBorrowings ? "Delete" : "Has active borrowings"
+                }
+                placement="top-start"
               >
-                <DeleteForeverIcon />
-              </Button>
+                <>
+                  <Button
+                    disabled={props.activeBorrowings}
+                    size="small"
+                    onClick={() => props.handleBookDelete(props.id)}
+                    variant="contained"
+                    color="secondary"
+                    style={{
+                      background: props.activeBorrowings
+                        ? "#FFC0C0"
+                        : "#E71919",
+                    }}
+                  >
+                    <DeleteForeverIcon />
+                  </Button>
+                </>
+              </Tooltip>
             </>
-          </Tooltip>
-          {!props.activeBorrowings ? (
-            <Button
-              onClick={() => props.handleAddBrowing(props.id)}
-              size="small"
-              variant="contained"
-              color="primary"
-            >
-              Add Borrowing
-              <AddCircleIcon style={{ marginLeft: "5px" }} />
-            </Button>
-          ) : (
-            <Button disabled size="small" variant="contained" color="primary">
-              Not Available
-              <BlockIcon style={{ marginLeft: "5px" }} />
-            </Button>
           )}
+          {!props.activeBorrowings ? (
+                token.role === "admin" ? <Button
+                  onClick={() => props.handleAddBrowing(props.id)}
+                  size="small"
+                  variant="contained"
+                  color="primary"
+                >
+                  Add Borrowing
+                  <AddCircleIcon style={{ marginLeft: "5px" }} />
+                </Button> : 
+                <Button
+                size="small"
+                variant="contained"
+                color="primary"
+              >
+                Available
+              </Button>
+              ) : (
+                <Button
+                  disabled
+                  size="small"
+                  variant="contained"
+                  color="primary"
+                >
+                  Not Available
+                  <BlockIcon style={{ marginLeft: "5px" }} />
+                </Button>
+              )}
         </CardActions>
       </Card>
     </Grid>
